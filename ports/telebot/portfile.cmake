d3e0869b234e3e9acea19d889f26566ad899a501
Dmitrii <81250499+KorablikDimak@users.noreply.github.com>
2024-09-27 01:25:08 +0300

    Update portfile.cmake

--- ports/telebot/portfile.cmake
+++ ports/telebot/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO KorablikDimak/ApiTelegramBot
         REF "${VERSION}"
-        SHA512 1d0892914a27317f990f4934f72f8fe19c47b9454fceb338f769c34b904a9ea0a94c3d9567e8b5bbf53dc57e1104db3b9c99abecd0e52e2d3f526e1c9df3d1a7
+        SHA512 f3799d32df5e2f0f3558875cb7aa8655db293dacb2f6b9b0b5a76f959def8097709368da9497b221b601c350fd2bca35d3c38ecf4d1a9af34c358120e9f00e8f
         HEAD_REF master)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
