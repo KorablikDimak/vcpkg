15c78108aabd0ad43c1c1c4773ee37d60bd13095
Dmitrii <81250499+KorablikDimak@users.noreply.github.com>
2024-09-27 01:28:42 +0300

    Update portfile.cmake

--- ports/openai/portfile.cmake
+++ ports/openai/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO KorablikDimak/ApiOpenAI
         REF "${VERSION}"
-        SHA512 be118e04168e4b5780c3ac44287b649b6d94bf732288bb3d073d4fe9fdf779cf318c0211dde7dbaccee16d9a1974b69fbaf6bda24a09ade83c03f8ada6a51c86
+        SHA512 87312647c24cfdc20475cc02761cda3eaffeb970cde048adecfc9f30dce70d8a0a4f21c9f4e75e1ebc654cdd8c6a918381e7f40b3f389a155abb0f00834b2e19
         HEAD_REF master)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
