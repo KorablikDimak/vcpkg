940744ca1eee6f1b5604772a8a64608799997b09
Dmitrii <81250499+KorablikDimak@users.noreply.github.com>
2024-10-17 23:53:23 +0300

    Update portfile.cmake

--- ports/extendedcpp/portfile.cmake
+++ ports/extendedcpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO KorablikDimak/ExtendedCpp
         REF "${VERSION}"
-        SHA512 0e677cd88c07d240d0ea55817253a97119bb8eff68f9686a96aab6db37b41c4bba234d826d26e44f4827ddfbfdd8c0baca9d4c45cd7d34bbe3dcdefb2e21ed19
+        SHA512 7cecbeff0e057c7ddd483263da71f7451c851e7347fe5e14ff46f87c8e0876711761a39a12ae601a982b186def97d0609a27164bab0b5330a5e35c60f7e51d94
         HEAD_REF master)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
