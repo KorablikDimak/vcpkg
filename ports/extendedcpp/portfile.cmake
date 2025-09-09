cf5ea60067ef6886f17ec17d8bf8ee96c254d975
KorablikDimak <korablikdimak@gmail.com>
2025-09-09 11:52:32 +0300

    add 2.2.5 extendedcpp

--- ports/extendedcpp/portfile.cmake
+++ ports/extendedcpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO KorablikDimak/ExtendedCpp
         REF "${VERSION}"
-        SHA512 6fa7b4cc387c44860f674d734b3fe519ba034fc89cb539f2463ff2a51c439187aa154528d2eedf9a79b59ed3c6f329c43fffc46e4ca5d02835854df427a57c6e
+        SHA512 26e975d7d95a1bcf7f9b8b69f35adf913c4ce6c1c3449c97a7c5d933013c91290f62c5fc6e0cf7669f11440311765eaf900134a345c92515fd1a50b5734d717b
         HEAD_REF master)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
